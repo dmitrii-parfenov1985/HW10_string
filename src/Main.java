51d6a60bc20bed5bbe5757090879b04bc628694c
Дмитрий Парфенов <parfyonovdv@gmail.com>
2025-01-05 11:50:52 +0300

    Home Work is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("\nЗадача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " +  middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("\nЗадача 2");
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println("\nЗадача 3");
+        String fullNameOne = "Иванов Семён Семёнович";
+        String replacedName = fullNameOne.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника - " + replacedName);
     }
 }
